76deb140e1ad20bedcbea5be61f58e591cc24aa3
Davidmartinezpunzano <32137991+Davidmartinezpunzano@users.noreply.github.com>
2017-10-19 17:55:34 +0200

    Add files via upload
    subida adicional de codigo

--- bicisrobadastest2.php
+++ bicisrobadastest2.php
@@ -1,34 +1,70 @@
 <?php
-
+	$model=$_REQUEST['modelo'];
+	$ciudad=$_REQUEST['ciudad'];
 	$marca=$_REQUEST['marca'];
 	if (($marca=="")OR(!isset($marca))) {
 		//header("Location: bicisrobadastest2.html");
 	}else{
 
 		//Creamos la conexion con la base de datos
-		$conexion=mysqli_connect("localhost:8080", "root", "", "bicisrobadas");
-
-		//Seleccionamos qué tabla escogeremos (PRODUCTO), qué columnas elegimos (todas: *) y con qué filtro: nombre
-		$j="SELECT * FROM anunci WHERE pro_marca LIKE '%$marca%'";
-
-		//Hacemos la consulta a la BDD, con la dirección de la BDD y con qué queremos consultar
-		$consulta = mysqli_query($conexion, $j);
-
-		//Creamos la variable mysqli_num_rows para mostrar cuántos objetos coinciden con la búsqueda
-		//$filas = mysqli_num_rows($consulta);
-
-		//Mostramos el número de objetos
-		//echo "$filas";
-
-		//Mostramos los detalles del producto
-		while($producto=mysqli_fetch_array($consulta)){
-			echo "ID: $producto[pro_id]<br/>";
-			echo "Nombre: $producto[pro_nombre] <br/>";
-			echo "Descripción: $producto[pro_descripcion]<br/>";
-			echo "Precio: $producto[pro_precio]€<br/>";
-			echo "Categoria: $producto[pro_categoria]<br/><br>";
+		$mysqli = new mysqli("localhost", "root", "", "bd_bicisrobadas");
+		$conexion=mysqli_connect("localhost", "root", "", "bd_bicisrobadas");
+			if(!$conexion){
+			    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+			    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+			    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+			    exit;
+			} else {
+		//La siguiente linea no es necesaria, simplemente la pondremos ahora para poder observar que la conexión ha sido realizada
+		echo 'Conectado  satisfactoriamente al servidor <br />';
 		}
-	}
 
+		$acentos=mysqli_query($conexion, "SET NAMES 'utf8'");
 
-?>
+		$sql = "SELECT anu_marca, anu_model, anu_ubicacio_robatori FROM anunci WHERE anu_marca = '$marca'";
+
+		//$result = mysql_query ($sql);
+
+		if (!$resultado = $mysqli->query($sql)) {
+    	// ¡Oh, no! La consulta falló. 
+   		echo "Lo sentimos, este sitio web está experimentando problemas.";
+
+    	// De nuevo, no hacer esto en un sitio público, aunque nosotros mostraremos
+    	// cómo obtener información del error
+    	echo "Error: La ejecución de la consulta falló debido a: \n";
+    	echo "Query: " . $sql . "\n";
+    	echo "Errno: " . $mysqli->errno . "\n";
+    	echo "Error: " . $mysqli->error . "\n";
+    	exit;
+}
+		if ($resultado->num_rows === 0) {
+    	// ¡Oh, no ha filas! Unas veces es lo previsto, pero otras
+    	// no. Nosotros decidimos. En este caso, ¿podría haber sido
+    	// actor_id demasiado grande? 
+    	echo "Lo sentimos. No se pudo encontrar una coincidencia para el ID $marca. Inténtelo de nuevo.";
+    	exit;
+}
+
+		echo "<table border='1'><tr><td>Marca</td><td>Modelo</td><td>Ubicacion</td>
+         </tr><tr>";
+	  while ($row = mysql_fetch_row($resultado)){
+                echo "<td>".$row[0]."</td><td>".$row[1]."</td>
+              <td>".$row[2]."</td>";
+   }
+   echo "</tr></table>";
+		/*if(mysqli_num_rows($resultados)>0){
+					while($producto = mysqli_fetch_array($resultados)){
+						echo "marca: $producto[anu_marca]<br/>";
+						echo "model: $producto[anu_model]<br/>";
+						echo "ciudad: $producto[anu_ciudad]<br/>";
+						//echo "Precio: $producto[pro_precio]<br/><br/>";
+					}
+				} else {
+					echo "No hay productos que mostrar";*/
+		//$sql = "SELECT anu_marca, anu_model, anu_ubicacio_robatori FROM anunci ORDER BY rand() LIMIT 5";
+		//$bici = $resultado->fetch_assoc();
+		//echo "A veces veo a " . $bici['$marca'] . " " . $bici['$modelo'] . " en la TV.";
+
+			}
+			
+		?>
