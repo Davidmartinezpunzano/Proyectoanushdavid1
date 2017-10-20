8725e5127e7de7d039f3225a4754b433073e8852
David <martinezpunzanodavid96@gmail.com>
2017-10-20 19:49:53 +0200

    Actualización del codigo 3.0

--- bicisrobadastest2.php
+++ bicisrobadastest2.php
@@ -2,6 +2,7 @@
 	$model=$_REQUEST['modelo'];
 	$ciudad=$_REQUEST['ciudad'];
 	$marca=$_REQUEST['marca'];
+	$color=$_REQUEST['color'];
 	if (($marca=="")OR(!isset($marca))) {
 		//header("Location: bicisrobadastest2.html");
 	}else{
@@ -21,7 +22,11 @@
 
 		$acentos=mysqli_query($conexion, "SET NAMES 'utf8'");
 
-		$sql = "SELECT anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio  FROM anunci WHERE anu_marca = '$marca'";
+		$sql = "SELECT anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio  FROM anunci WHERE anu_marca = '$marca' and anu_model = '$model' and anu_ubicacio_robatori = '$ciudad' and anu_color = '$color'";
+
+				  
+		
+
 
 		if (!$resultado = $mysqli->query($sql)) {
     	// ¡Oh, no! La consulta falló. 
@@ -39,18 +44,33 @@
     	// ¡Oh, no ha filas! Unas veces es lo previsto, pero otras
     	// no. Nosotros decidimos. En este caso, ¿podría haber sido
     	// actor_id demasiado grande? 
-    	echo "Lo sentimos. No se pudo encontrar una coincidencia para el ID $marca. Inténtelo de nuevo.";
+    	echo "Lo sentimos. No se pudo encontrar una coincidencia con la marca $marca. Inténtelo de nuevo.";
     	exit;
 }
+		$totalFilas    =    mysqli_num_rows($resultado);
+		echo "Resultados encontrados: $totalFilas";
 
-		echo "<table border='1'><tr><td>ID</td><td>Titulo</td><td>Fecha de anuncio</td><td>Fecha del robo</td><td>Ubicacion del robo</td><td>Descripcion del robo</td><td>Marca</td><td>Modelo</td><td>Color</td><td>Antigüedad de la bici</td><td>Descripcion</td><td>numero de serie</td><td>Foto</td><td>Compensacion</td>
-         </tr><tr>";
+		//echo "<table border='1'><tr><td>ID</td><td>Titulo</td><td>Fecha de anuncio</td><td>Fecha del robo</td><td>Ubicacion del robo</td><td>Descripcion del robo</td><td>Marca</td><td>Modelo</td><td>Color</td><td>Antigüedad de la bici</td><td>Descripcion</td><td>numero de serie</td><td>Foto</td><td>Compensacion</td>
+         //</tr><tr>";
+         //echo "</tr></table">;
+         $contador=0;
+
+         echo "<table border='1'>";
+         for ($contador=0; $contador<=$totalFilas ; $contador++) { 
+         	
+         
 	  while ($row = mysqli_fetch_row($resultado)){
-                echo "<td>".$row[0]."</td><td>".$row[1]."</td>
-              <td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td><td>".$row[5]."</td><td>".$row[6]."</td><td>".$row[7]."</td><td>".$row[8]."</td><td>".$row[9]."</td><td>".$row[10]."</td><td>".$row[11]."</td><td>".$row[12]."</td><td>".$row[13]."</td>";
-   }
+	  			echo "<table border='1'><tr>";
+                echo "<td><img src='$row[12]'></td><td> $row[0] $row[1] $row[2] $row[3] $row[4] $row[5] Fecha: $row[6] $row[7] $row[8] $row[9] $row[10] $row[11] $row[13] </td>";
+              }
+   
    echo "</tr></table>";
-	
+				echo "<br>";
+				echo "<br>";
+				}
 			}
+
 			
+
+
 		?>
